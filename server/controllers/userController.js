24a60ba3f4b88dc46a17f7ee0b1e2076eb0ef384
Kirill Sezen <kirill_sezen@mail.ru>
2026-08-19 12:16:37 +0300

    add checkRole and auth middlewares

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,55 @@
 const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const { User, Basket } = require('../models/models')
+const jwt = require('jsonwebtoken')
+
+const generateJwt = (id, email, role) => {
+	return jwt.sign({id, email, role}, process.env.JWT_SECRET, {expiresIn: '2h'})
+}
 
 class UserController {
-	async registration(req, res) {
+	async registration(req, res, next) {
+		const { email, password, role } = req.body
+		if (!email || !password) {
+			return next(ApiError.badRequest('Некорректный email или password'))
+		}
 
+		const candidate = await User.findOne({where: {email}})
+		if (candidate) {
+			return next(ApiError.badRequest('Пользователь с таким email уже зарегистрирован'))
+		}
+
+		const hashPassword = await bcrypt.hash(password, 5)
+
+		const user = await User.create({email, role, password: hashPassword})
+		const basket = await Basket.create({userId: user.id})
+
+		const jwt = generateJwt(user.id, user.email, user.role)
+
+		return res.json({jwtToken: jwt})
 	}
 
-	async login(req, res) {
+	async login(req, res, next) {
+		const { email, password } = req.body
 
+		const user = await User.findOne({where: {email}})
+		if (!user) {
+			return next(ApiError.badRequest('Пользователь с данным email отсутствует'))
+		}
+
+		const comparePassword = await bcrypt.compare(password, user.password)
+		if (!comparePassword) {
+			return next(ApiError.badRequest('Неверный пароль'))
+		}
+
+		const jwt = generateJwt(user.id, user.email, user.role)
+
+		return res.json({jwtToken: jwt})
 	}
 
 	async check(req, res, next) {
-		const {id} = req.query
-		if (!id) {
-			return next(ApiError.badRequest('id отсутствует'))
-		}
-		res.json(req.query)
+		const token = generateJwt(req.user.id, req.user.email, req.user.role)
+		res.json({jwtToken: jwt})
 	}
 }
 
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,5 +1,6 @@
 const Router = require('express')
 const userController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
 
 const router = new Router()
 
@@ -7,6 +8,6 @@ router.post('/registration', userController.registration)
 
 router.post('/login', userController.login)
 
-router.get('/auth', userController.check)
+router.get('/auth', authMiddleware, userController.check)
 
 module.exports = router
